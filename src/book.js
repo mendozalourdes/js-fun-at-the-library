60837b4d91f7bd6c59eaa4b2acee166487bf6099
Lourdes Mendoza <lourd.mendoza@gmail.com>
2021-03-24 18:36:40 -0600

    Create new function

--- src/book.js
+++ src/book.js
@@ -1,6 +1,34 @@
+// createTitle
+//   ✓ should be a function
+//   - should take in a title and return a modified title
+//   - should be able to create many modified titles
+
+
+
 function createTitle() {
-  
+  var bookIdea = "Storm's Awakening";
+
+  return `The ${bookIdea}`
 }
+var sushiTitle = "Dancing Sushi";
+var dragonTitle = "Dragon in the Summer";
+var ghostTitle = "Teenage Ghoul";
+
+
+function createTitle(book) {
+  return `The ${book}`
+}
+
+// it.skip("should be able to create many modified titles", function () {
+//   var sushiTitle = createTitle("Dancing Sushi");
+//   var dragonTitle = createTitle("Dragon in the Summer");
+//   var ghostTitle = createTitle("Teenage Ghoul");
+//
+//   assert.equal(sushiTitle, "The Dancing Sushi");
+//   assert.equal(dragonTitle, "The Dragon in the Summer");
+//   assert.equal(ghostTitle, "The Teenage Ghoul");
+// });
+// });
 
 module.exports = {
   createTitle,
